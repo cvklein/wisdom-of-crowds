4a74db8c559250ebbcc189782265b8703cb02194
marc cheong <mc@marcs-MacBook-Pro.local>
2021-10-20 11:32:09 +1100

    added pydoc, cleaned up, converted helper fns to internal private fns (__ underscored)

--- wisdom_of_crowds.py
+++ wisdom_of_crowds.py
@@ -4,33 +4,55 @@ import itertools
 from networkx.exception import NetworkXNoPath
 
 class Crowd:
-    def __init__(self,G,max_m=5,node_key='T'):
+""" 
+Class for encapsulating a graph and pre-computed (memoized) features for the 
+Wisdom of Crowds algorithm, per Sullivan et al. (2020),
+"Vulnerability in Social Epistemic Networks" *International Journal of Philosophical Studies* 
+https://doi.org/10.1080/09672559.2020.1782562
+:param G: a networkx graph, typically DiGraph.
+:param max_m: maximum m to consider in the calculations
+:param node_key: attribute to consider for each node, when considering topic diversity (defaults to 'T')
+"""
+    def __init__(self, G, max_m=5, node_key='T'):
         self.G = G
         self.min_k = 2
         self.max_k = 5
         self.min_m = 1
-        self.max_m=max_m
-        self.node_key='T'
-        self.precomputed_path_dict = {} #holds unconditional paths
-        self.precomputed_paths_by_hole_node = defaultdict(dict)  #holds dict of paths per node
+        self.max_m = max_m
+        self.node_key = 'T'
+        self.precomputed_path_dict = {} # holds unconditional paths
+        self.precomputed_paths_by_hole_node = defaultdict(dict)  # holds dict of paths per node
         self.node_set = set(G.nodes())
 
 
-    #makes search for possible cliques more efficient
-    def efficient_pairs(self,x):
+    def __efficient_pairs(self, x):
+        """
+        __efficient_pairs: internal function, makes search for possible cliques more efficient.
+            This should not be called directly by the user.
+        :param x: input list
+        :returns: unique pairs of elements (x_i, x_j)
+        """
         l = len(x)
-        for i in range(1,l):
-            for j in range(0,i):
-                yield (x[i],x[j])
-
-    #for a single node, and a single source, precompute the path
-    #and populate the current dictionary with them.
-
-    def shortest_path_node_source_target(self,v,source,target):
+        for i in range(1, l):
+            for j in range(0, i):
+                yield (x[i], x[j])
 
 
-
-        #step 1: am I in the generic path dictionary?
+    def __shortest_path_node_source_target(self, v, source, target):
+        """
+        __shortest_path_node_source_target: internal function,
+            to get the length of the shortest path between vertices source and target, excluding v. 
+            The results need to be postprocessed by wrapper function 
+            shortest_path_length_node_source_target and not called directly.
+            This function does memoization for efficient future processing:
+            for a single node, and a single source, precompute the path,
+            and populate the current dictionary precomputed_path_dict with them.
+        :param v: vertex under exclusion
+        :param source: source node
+        :param target: target node
+        :returns: integer z, in range 0 <= z < +infinity (unadjusted)
+        """
+        # step 1: am I in the generic path dictionary? (memoized)
         try:
             shortest_unconditional_path = self.precomputed_path_dict[(source,target)]
         except KeyError: #well figure this later in case it comes in handy
@@ -44,11 +66,11 @@ class Crowd:
                     z = shortest_unconditional_path[x:]
                     self.precomputed_path_dict[(z[1],target)] = z
 
-        #step 2 check if this is also a path without the node of interest
+        # step 2 check if this is also a path without the node of interest
         if v not in shortest_unconditional_path:
             return shortest_unconditional_path
-        else: #now we have to find the shortest path in a subgraph without the node of interest
-
+        # now we have to find the shortest path in a subgraph without the node of interest    
+        else: 
             try:
                 shortest_conditional_path = self.precomputed_paths_by_hole_node[v][(source,target)]
                 return shortest_conditional_path
@@ -58,58 +80,69 @@ class Crowd:
                 G_sub = self.G.subgraph(nodes_less_v)
                 try:
                     shortest_conditional_path = nx.algorithms.shortest_path(G_sub,source,target)
-                    #note that subpaths could also be cached as per above
+                    # note that subpaths could also be cached as per above
                     self.precomputed_paths_by_hole_node[v][(source,target)] = shortest_conditional_path
                     return shortest_conditional_path
                 except NetworkXNoPath:
                     self.precomputed_paths_by_hole_node[v][(source,target)] = []
                     return []
 
-    #wrapper function to get the length. no path = infinite length
-    def shortest_path_length_node_source_target(self,v,source,target):
-        z = len(self.shortest_path_node_source_target(v,source,target))
+    
+    def shortest_path_length_node_source_target(self, v, source, target):
+        """
+        shortest_path_length_node_source_target: wrapper function to get the length of the
+            shortest path between vertices source and target, without vertex v. 
+            no path = infinite length
+        :param v: vertex under consideration, as defined by (Sullivan et al., 2020)
+        :param source: source node
+        :param target: target node
+        :returns: integer z, in range 0 <= z <= +infinity
+        """
+        z = len(self.__shortest_path_node_source_target(v, source, target))
         if z == 0:
             return(float('inf'))
         else:
             return z - 1
 
 
-
-
-    def is_mk_observer(self,v,m,k):
-
-
-
+    def is_mk_observer(self, v, m, k):
+        """
+        is_mk_observer: checks if the vertex v is an (m,k)-observer as defined by (Sullivan et al., 2020)
+            optimized clique-finding algo by CVK
+        :param v: vertex to evaluate
+        :param m: m as defined in (Sullivan et al., 2020)
+        :param k: k as defined in (Sullivan et al., 2020)
+        :returns: boolean 
+        """
         source_nodes = list(self.G.predecessors(v))
 
-
-        if len(source_nodes) < k: #if you have fewer than k, then you can't hear from at least k
+        # if you have fewer than k, then you can't hear from at least k
+        if len(source_nodes) < k: 
             return False
 
-        if (len(source_nodes) == 1) and k==1 and m==1: #special case, to ensure that a node with one input is a 1,1 observer
+        # special case, to ensure that a node with one input is a 1,1 observer
+        if (len(source_nodes) == 1) and k==1 and m==1: 
             return True
 
         max_k_found = False
-        clique_dict = defaultdict(list) #this will get used to look for cliques
+        clique_dict = defaultdict(list) # this will get used to look for cliques
 
-        #efficient_pairs makes sure that cliques are found and early termination happens
-        #as soon as possible
-        for source_a,source_b in self.efficient_pairs(source_nodes):
-
+        # helper method __efficient_pairs makes sure that cliques are found and 
+        # early termination happens as soon as possible
+        for source_a,source_b in self.__efficient_pairs(source_nodes):
             a_path_length = self.shortest_path_length_node_source_target(v,source_a,source_b)
             b_path_length = self.shortest_path_length_node_source_target(v,source_b,source_a)
 
-
-
-
-            if (a_path_length<m) or (b_path_length<m): #if shortest path is too short, keep looking
+            # if shortest path is too short, keep looking
+            if (a_path_length<m) or (b_path_length<m): 
                 pass
 
             else:  # now we do the clique updating
-                #first each pair trivially forms a clique
-                #pairs are unique so we don't have to double-check as we go (i hope!)
+                # first each pair trivially forms a clique
+                # pairs are unique so we don't have to double-check as we go (i hope!)
 
-                #first, this check is needed because if k<=2 then any hit at all satisfies it and it's time to go home
+                # first, this check is needed because if k<=2 then any hit at all satisfies it;
+                # and it's time to go home
                 if k<=2:
                     return True
 
@@ -118,50 +151,62 @@ class Crowd:
                 clique_dict[source_b].append(trivial_clique)
 
 
-            #now, for each pair of cliques for the two nodes, we have a new clique iff:
-            #each clique has the same size m
-            #the set containing the union of nodes from the two pairs of m-sized cliques is size m+1
-            #so check the cliques in the nodes connected by the new pair
-                for a,b in itertools.product(clique_dict[source_a],clique_dict[source_b]):
+            # now, for each pair of cliques for the two nodes, we have a new clique iff:
+            # each clique has the same size m
+            # the set containing the union of nodes from the two pairs of m-sized cliques is size m+1
+            # so check the cliques in the nodes connected by the new pair
+                for a, b in itertools.product(clique_dict[source_a], clique_dict[source_b]):
                     lena = len(a)
                     lenb = len(b)
-                    if lena!=lenb:
+                    if lena != lenb:
                         pass
-                    #avoid double counting
-                    #thogh you can probably do this faster by not adding the trivial clique until later?
-                    elif (a==trivial_clique) or (b==trivial_clique):
+                    # avoid double counting
+                    # thogh you can probably do this faster by not adding the trivial clique until later?
+                    elif (a == trivial_clique) or (b == trivial_clique):
                         pass
                     else:
                         node_union = a | b
                         lenu = len(node_union)
                         if lenu == (lena + 1):
-                            if lenu>=k:  #early termination
+                            if lenu >= k:  # early termination
                                 max_k_found = True
                                 return max_k_found
                             else:
                                 for node in node_union:
                                     clique_dict[node].append(node_union)
-
-
-
         return max_k_found
 
 
-    def S(self,v):
-        possibilities = sorted([(m*k,m,k) for m,k in itertools.product(range(self.min_m,self.max_m+1),range(self.min_k,self.max_k+1))],reverse=True)
-        for mk,m,k in possibilities:
-            mk_observer = self.is_mk_observer(v,m,k)
+    def S(self, v):
+        """
+        S: calculates S, defined in (Sullivan et al., 2020) as the structural position of v
+            S = max_{(m,k) in MK}(m * k)
+        :param v: vertex to evaluate
+        :returns: integer S, in range 0 <= (class constant max_m * class constant max_k) 
+        """
+        possibilities = sorted([(m*k, m, k) for m, k in \
+            itertools.product(range(self.min_m, self.max_m+1), \
+                              range(self.min_k, self.max_k+1))], \
+            reverse=True)
+        
+        for mk, m, k in possibilities:
+            mk_observer = self.is_mk_observer(v, m, k)
             if mk_observer:
                 return mk
             else:
                 pass
-
         return 0
 
 
-    def D(self,v):
+    def D(self, v):
+        """
+        D: calculates D, defined in the literature as the number of topics found for
+            informants of vertex v per (Sullivan et al., 2020)
+            We apply the general case D = D' = | union_{(u,v) in E} C'(u) |
+        :param v: vertex to evaluate
+        :returns: integer D, in range 0 <= D 
+        """
         topics = set()
-
         source_nodes = self.G.predecessors(v)
 
         for s in source_nodes:
@@ -171,21 +216,26 @@ class Crowd:
             else:
                 topics.add(s_topic)
 
-
-
-
         return len(topics)
 
 
+    def pi(self, v):
+        """
+        pi: calculates pi, given vertex v, defined in (Sullivan et al., 2020) as the product of S and D
+        :param v: vertex to evaluate
+        :returns: integer pi, where pi = S * D
+        """
+        return self.D(v) * self.S(v)
 
 
-    def pi(self,v):
-        return  self.D(v) * self.S(v)
-
-    def h_measure(self,v,max_h=6):
-
+    def h_measure(self, v, max_h=6):
+        """
+        h_measure: find the highest h, given vertex v, of which mk_observer(v, h, h) is true
+        :param v: vertex to evaluate
+        :param max_h: maximum_h to evaluate, defaults to 6 per the literature 
+        :returns: integer h, in range 1 <= h <= max_h 
+        """
         for h in range(max_h,0,-1):
             if self.is_mk_observer(v,h,h):
                 return h
-
         return 0
