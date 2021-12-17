b7cd8b28153fc08d47b396fd0a623ad8d54b5271
marc cheong <mc@marcs-MacBook-M1.local>
2021-12-17 14:03:22 +1100

    added autotest and badging script

---
+++ tests.sh
@@ -0,0 +1,5 @@
+pytest --junitxml=.reports/junit.xml
+genbadge tests -v -i .reports/junit.xml -o .reports/tests-badge.svg
+coverage run -m pytest
+coverage xml -o .reports/coverage.xml wisdom_of_crowds.py
+genbadge coverage -v -i .reports/coverage.xml -o .reports/coverage-badge.svg
