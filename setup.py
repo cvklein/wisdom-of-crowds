6e0994fee1e41c883f5f41631a20107b2dec256e
marc cheong <mc@marcsmacbookm1.mobility.unimelb.net.au>
2022-05-11 16:27:13 +1000

    fixed MANIFEST and gitignore for clean distribution and git push

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='wisdom_of_crowds',
+    version='1.01',
+    description='Re-implementation and extension of the measures described in Sullivan et al. (2020): Vulnerability in Social Epistemic Networks, International Journal of Philosophical Studies',
+    license='GPL3.0',
+    author="Colin Klein and Marc Cheong",
+    author_email='Colin.Klein@anu.edu.au',
+    packages=find_packages('wisdom_of_crowds'),
+    package_dir={'': 'src'},
+    url='https://github.com/cvklein/wisdom-of-crowds',
+    keywords='wisdom crowds epistemology network',
+    install_requires=[
+          'networkx',
+          'matplotlib'
+      ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Sociology',
+    ],
+    python_requires='>=3.7',
+)
